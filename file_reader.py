39fa38625f3ed57082a5f88a2b5511a226b2b808
Manuel Beisser <manuel.beisser@gmx.net>
2025-10-21 02:56:48 +0200

    added todo and length print

--- file_reader.py
+++ file_reader.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import requests
 
+# TODO: combine names and stations
+# TODO: write output function
 
 def fetcher(url: str) -> dict:
     response = requests.get(url)
     return response.json()
 
-def status_cleaner(status: dict):
+def status_cleaner(status: dict) -> dict:
     stations = status["data"]["stations"]
     station_dict = dict()
     for station in stations:
-        station_id = station.get("station_id")  #str
-        avail_bikes = station.get("num_bikes_available")  #int
-        rent_status = station.get("is_renting")  #bool
-        return_status = station.get("is_returning")  #bool
+        station_id = station.get("station_id")  # str
+        avail_bikes = station.get("num_bikes_available")  # int
+        rent_status = station.get("is_renting")  # bool
+        return_status = station.get("is_returning")  # bool
 
         station_id_dict = {
                 "avail_bikes": avail_bikes,
@@ -29,3 +31,4 @@ if __name__ == "__main__":
     information = fetcher("https://api.wstw.at/gateway/WL_WIENMOBIL_API/1/station_information.json")
 
     print(status_cleaner(status))
+    print(len(status_cleaner(status)))

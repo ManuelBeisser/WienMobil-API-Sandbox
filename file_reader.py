247b8eea1ea6e5e77f9e3367b6466245305fca1e
Manuel Beisser <manuel.beisser@gmx.net>
2025-10-21 09:30:19 +0200

    added information_cleaner

--- file_reader.py
+++ file_reader.py
@@ -4,9 +4,11 @@ import requests
 # TODO: combine names and stations
 # TODO: write output function
 
+
 def fetcher(url: str) -> dict:
     response = requests.get(url)
     return response.json()
+
 
 def status_cleaner(status: dict) -> dict:
     stations = status["data"]["stations"]
@@ -26,9 +28,26 @@ def status_cleaner(status: dict) -> dict:
     return station_dict
 
 
+def information_cleaner(information: dict) -> dict: 
+    names = information["data"]["stations"]
+    names_dict = dict()
+    for name in names:
+        station_id = name.get("station_id")  # str
+        station_name = name.get("name")  # str
+        station_capacity = name.get("capacity")  # int
+
+        names_id_dict = {
+            "station_name": station_name, 
+            "station_capacity": station_capacity
+        }
+        names_dict[station_id] = names_id_dict
+    return names_dict
+
 if __name__ == "__main__":
     status = fetcher("https://api.wstw.at/gateway/WL_WIENMOBIL_API/1/station_status.json")
     information = fetcher("https://api.wstw.at/gateway/WL_WIENMOBIL_API/1/station_information.json")
 
     print(status_cleaner(status))
     print(len(status_cleaner(status)))
+    print(information_cleaner(information))
+    print(len(information_cleaner(information)))

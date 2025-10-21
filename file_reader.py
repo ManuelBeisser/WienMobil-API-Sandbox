36c9a7cd688bf75c2cc16a0e38f9a0d6094dd51c
Manuel Beisser <manuel.beisser@gmx.net>
2025-10-21 02:49:00 +0200

    status cleaner works

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,31 @@
+import pandas as pd
 import requests
 
 
-def fetcher(url: str):
+def fetcher(url: str) -> dict:
     response = requests.get(url)
     return response.json()
 
-api_url = "https://api.wstw.at/gateway/WL_WIENMOBIL_API/1/station_status.json"
-print(fetcher(api_url))
+def status_cleaner(status: dict):
+    stations = status["data"]["stations"]
+    station_dict = dict()
+    for station in stations:
+        station_id = station.get("station_id")  #str
+        avail_bikes = station.get("num_bikes_available")  #int
+        rent_status = station.get("is_renting")  #bool
+        return_status = station.get("is_returning")  #bool
+
+        station_id_dict = {
+                "avail_bikes": avail_bikes,
+                "rent_status": rent_status,
+                "return_status": return_status
+        }
+        station_dict[station_id] = station_id_dict
+    return station_dict
+
+
+if __name__ == "__main__":
+    status = fetcher("https://api.wstw.at/gateway/WL_WIENMOBIL_API/1/station_status.json")
+    information = fetcher("https://api.wstw.at/gateway/WL_WIENMOBIL_API/1/station_information.json")
+
+    print(status_cleaner(status))
